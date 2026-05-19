ababc40d5c5ca2005f1629316ecc6b89e4e0c166
Le.i <220001650+le-iy@users.noreply.github.com>
2026-05-19 16:09:09 +0800

    Create SA2_3_skills.php

---
+++ SA2_3_skills.php
@@ -0,0 +1,17 @@
+<h3>Skills</h3>
+
+<div class="skills-box">
+    <div>
+        <p><b>Hard Skills:</b></p>
+        <p>HTML</p>
+        <p>CSS</p>
+        <p>Basic PHP</p>
+        <p>Basic Programming</p>
+    </div>
+
+    <div>
+        <p><b>Soft Skills:</b></p>
+        <p>Teamwork</p>
+        <p>Problem Solving</p>
+    </div>
+</div>
