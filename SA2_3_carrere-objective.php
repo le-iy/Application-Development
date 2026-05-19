b27cbb2ae8a1f00ef98777fef928c5c591fe7792
Le.i <220001650+le-iy@users.noreply.github.com>
2026-05-19 16:09:00 +0800

    Create SA2_3_carrere-objective.php

---
+++ SA2_3_carrere-objective.php
@@ -0,0 +1,7 @@
+<h3>Career Objective</h3>
+
+<p>
+    To apply my knowledge in Application Development by showcasing my skills
+    in developing meaningful portfolios and innovating systems, while improving
+    my technical skills through academic projects and hands-on activities.
+</p>
