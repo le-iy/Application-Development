a1efd3aec10155033333afa54329fe18c1404ddf
Le.i <220001650+le-iy@users.noreply.github.com>
2026-05-19 14:28:31 +0800

    Update FA4_1_INDEX.php

--- FA4_1_INDEX.php
+++ FA4_1_INDEX.php
@@ -1,32 +1,30 @@
 <html>
 <head>
-    <title>Character Biography Collection</title>
+    <title>Fave Genshin Chars Bio</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="FA4_css.css">
 </head>
 
 <body>
 
     <header>
-        <h1>Character Biography Collection</h1>
-        <p>Five short biographies loaded using PHP require function.</p>
+        <p class="small-title">Genshin Impact Faves</p>
+        <h1>CHARACTER BIOGRAPHY</h1>
+        <p class="header-text">
+            Formative Assessment 4: 1 - Short Story Webpgae
+        </p>
     </header>
 
-    <div class="container">
-
-        <?php require 'FA4_1_bio1.php'; ?>
-
-        <?php require 'FA4_1_bio2.php'; ?>
-
-        <?php require 'FA4_1_bio3.php'; ?>
-
+    <div class="story-container">
+        <?php include 'FA4_1_bio1.php'; ?>
+        <?php include 'FA4_1_bio2.php'; ?>
+        <?php include 'FA4_1_bio3.php'; ?>
         <?php require 'FA4_1_bio4.php'; ?>
-
         <?php require 'FA4_1_bio5.php'; ?>
-
     </div>
 
     <footer>
-        <p>PHP Predefined Functions Activity</p>
+        <p>&copy; <a href="https://genshin-impact.fandom.com/wiki/Genshin_Impact_Wiki" target="_blank">Genshin Impact Wiki</a></p>
     </footer>
 
 </body>
