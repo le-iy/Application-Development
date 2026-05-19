4a2e4ed13807e8edcbed944a14bc6ddb3634d44d
Le.i <220001650+le-iy@users.noreply.github.com>
2026-05-19 14:28:24 +0800

    Update FA4_1_bio2.php

--- FA4_1_bio2.php
+++ FA4_1_bio2.php
@@ -1,13 +1,13 @@
-<div class="bio-card">
-    <img src="images/character2.jpg" alt="Character 2">
+<div class="story-box">
+    <img src="image_bank/Venti_Icon.webp">
 
-    <div class="bio-text">
-        <h2>Character Name 2</h2>
+    <h3>Venti</h3>
 
-        <p>
-            Character Name 2 is a creative and intelligent character who enjoys solving problems.
-            They are often seen helping others and thinking of new ideas.
-            This character represents curiosity, teamwork, and confidence.
-        </p>
-    </div>
+    <p>
+        A bard that seems to have arrived on some unknown wind — sometimes
+        sings songs as old as the hills, and other times sings poems fresh
+        and new. When using his Anemo power to control the wind, it often
+        appears as feathers, as he's fond of that which appears light and
+        breezy.
+    </p>
 </div>
