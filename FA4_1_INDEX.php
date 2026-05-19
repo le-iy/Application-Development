a702d649519e89067f005d047f71f98e9f17d95e
Le.i <220001650+le-iy@users.noreply.github.com>
2026-05-19 12:41:33 +0800

    Create FA4_1_INDEX.php

---
+++ FA4_1_INDEX.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+    <title>Character Biography Collection</title>
+    <link rel="stylesheet" href="FA4_css.css">
+</head>
+
+<body>
+
+    <header>
+        <h1>Character Biography Collection</h1>
+        <p>Five short biographies loaded using PHP require function.</p>
+    </header>
+
+    <div class="container">
+
+        <?php require 'FA4_1_bio1.php'; ?>
+
+        <?php require 'FA4_1_bio2.php'; ?>
+
+        <?php require 'FA4_1_bio3.php'; ?>
+
+        <?php require 'FA4_1_bio4.php'; ?>
+
+        <?php require 'FA4_1_bio5.php'; ?>
+
+    </div>
+
+    <footer>
+        <p>PHP Predefined Functions Activity</p>
+    </footer>
+
+</body>
+</html>
