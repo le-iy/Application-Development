298234b2ea9d23b61fdc44b9f9c038e40d4bb0fc
Le.i <220001650+le-iy@users.noreply.github.com>
2026-05-19 16:09:07 +0800

    Create SA2_3_personal-info.php

---
+++ SA2_3_personal-info.php
@@ -0,0 +1,7 @@
+<h2>Personal Information</h2>
+
+<p><b>Name:</b> Lei Caabay</p>
+<p><b>Age:</b> 21</p>
+<p><b>Address:</b> Sampaloc City, Manila</p>
+<p><b>Email:</b> lecaabay@fit.edu.ph</p>
+<p><b>Contact Number:</b> 09278067775</p>
