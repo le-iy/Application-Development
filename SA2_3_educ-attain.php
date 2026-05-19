03bee28616e95db96a4dca020fc16195369e19f6
Le.i <220001650+le-iy@users.noreply.github.com>
2026-05-19 16:09:02 +0800

    Create SA2_3_educ-attain.php

---
+++ SA2_3_educ-attain.php
@@ -0,0 +1,6 @@
+<h3>Educational Attainment</h3>
+
+<p><b>Course:</b> Bachelor of Science in Information Technology</p>
+<p><b>School:</b> FEU Institute of Technology</p>
+<p><b>Year Level:</b> 2nd Year</p>
+<p><b>Senior High School:</b> Holy Trinity University</p>
