373a444c4106af85fe4882c3828ba7630439ed6b
Le.i <220001650+le-iy@users.noreply.github.com>
2026-06-15 17:33:31 +0800

    Update ResultColors.php

--- FA5/number3/ResultColors.php
+++ FA5/number3/ResultColors.php
@@ -1,6 +1,16 @@
 <?php
-    session_start();
+session_start();
 
+if (
+    !isset($_SESSION["fave_color1"]) ||
+    !isset($_SESSION["fave_color2"]) ||
+    !isset($_SESSION["fave_color3"]) ||
+    !isset($_SESSION["fave_color4"]) ||
+    !isset($_SESSION["fave_color5"])
+) {
+    header("Location: FavoriteColor.php");
+    exit();
+}
 ?>
 
 <html>
@@ -21,11 +31,25 @@
         
         <h3>My Favorite Colors</h3>
 
-        <div>My Favorite Color 1: <?php echo $_SESSION["fave_color1"]; ?></div>
-        <div>My Favorite Color 2: <?php echo $_SESSION["fave_color2"]; ?></div>
-        <div>My Favorite Color 3: <?php echo $_SESSION["fave_color3"]; ?></div>
-        <div>My Favorite Color 4: <?php echo $_SESSION["fave_color4"]; ?></div>
-        <div>My Favorite Color 5: <?php echo $_SESSION["fave_color5"]; ?></div>
+        <div style="color: <?php echo $_SESSION["fave_color1"]; ?>;">
+            My Favorite Color 1: <?php echo $_SESSION["fave_color1"]; ?>
+        </div>
+
+        <div style="color: <?php echo $_SESSION["fave_color2"]; ?>;">
+            My Favorite Color 2: <?php echo $_SESSION["fave_color2"]; ?>
+        </div>
+
+        <div style="color: <?php echo $_SESSION["fave_color3"]; ?>;">
+            My Favorite Color 3: <?php echo $_SESSION["fave_color3"]; ?>
+        </div>
+
+        <div style="color: <?php echo $_SESSION["fave_color4"]; ?>;">
+            My Favorite Color 4: <?php echo $_SESSION["fave_color4"]; ?>
+        </div>
+
+        <div style="color: <?php echo $_SESSION["fave_color5"]; ?>;">
+            My Favorite Color 5: <?php echo $_SESSION["fave_color5"]; ?>
+        </div>
     </div>
 </div>
 
