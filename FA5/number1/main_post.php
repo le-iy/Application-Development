80518d042d7a8f765c34c8c32170e208aaed9844
Le.i <220001650+le-iy@users.noreply.github.com>
2026-06-15 16:06:02 +0800

    Create main_post.php

---
+++ FA5/number1/main_post.php
@@ -0,0 +1,67 @@
+<html>
+<head>
+    <title>Formative 5</title>
+    <link rel="stylesheet" href="../fa5_css.css">
+</head>
+<body>
+
+<div class="container">
+    <h2>Formative 5</h2>
+    <p class="subtitle">Number 2</p>
+
+    <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+        <div class="form-row">
+            <label>First Name <span class="required_symbl">*</span></label>
+            <input type="text" name="firstname" placeholder="Enter your first name">
+        </div>
+
+        <div class="form-row">
+            <label>Middle Name <span class="required_symbl">*</span></label>
+            <input type="text" name="middlename" placeholder="Enter your middle name">
+        </div>
+
+        <div class="form-row">
+            <label>Last Name <span class="required_symbl">*</span></label>
+            <input type="text" name="lastname" placeholder="Enter your last name">
+        </div>
+
+        <div class="form-row">
+            <label>Date of Birth <span class="required_symbl">*</span></label>
+            <input type="text" name="birthdate" placeholder="Month Day Year">
+        </div>
+
+        <div class="form-row">
+            <label>Address <span class="required_symbl">*</span></label>
+            <input type="text" name="address" placeholder="Enter your address">
+        </div>
+
+        <div class="button-row">
+            <input type="submit" name="submitbtn" value="Submit">
+        </div>
+    </form>
+
+    <?php
+        if (isset($_POST['submitbtn'])) {
+            $fname = $_POST['firstname'];
+            $mname = $_POST['middlename'];
+            $lname = $_POST['lastname'];
+            $bday = $_POST['birthdate'];
+            $addr = $_POST['address'];
+    ?>
+
+    <div class="result-box">
+        <h3>Submitted Information</h3>
+        <div>First Name: <?php echo $fname; ?></div>
+        <div>Middle Name: <?php echo $mname; ?></div>
+        <div>Last Name: <?php echo $lname; ?></div>
+        <div>Date of Birth: <?php echo $bday; ?></div>
+        <div>Address: <?php echo $addr; ?></div>
+    </div>
+
+    <?php
+    }
+    ?>
+</div>
+
+</body>
+</html>
